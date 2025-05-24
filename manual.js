7576421e4f75f2a79fd7ae9517ab1fe1dc9b58ad
Monisharp-dev <hubdickson@gmail.com>
2025-05-24 06:52:20 +0100

    Add files via upload

--- manual.js
+++ manual.js
@@ -1,6 +1,6 @@
 // List of allowed user Ids
 const allowedIds = ["Ulingsmart102008","raetharolin","ajaniisraelojasope11","opalekesoromdayo","obunsodaniel","bilkisumusa888","feargodthompson","dconfidence322"
-"icefieldwaripamoweifreedom","utomobongpatrick16","sesughdaniel433","friedsteak006","owolabidamilare377","godswillg057","crys9213","adarikucecilia2","ezeisaiah2023","geoffreyjootar","paschalifechukwu2006","kehindeige527","emmakani234","agujiobisamuel","josephjoeboy445","mujaheedpresdor001"]; // Replace with your specific Ids
+"icefieldwaripamoweifreedom","utomobongpatrick16","sesughdaniel433","friedsteak006","owolabidamilare377","godswillg057","crys9213","adarikucecilia2","ezeisaiah2023","geoffreyjootar","paschalifechukwu2006","kehindeige527","emmakani234","agujiobisamuel","josephjoeboy445","mujaheedpresdor001","bokoh5061","ebubechiwinner066"]; // Replace with your specific Ids
 
 // Get the current user's Id from localStorage
 const currentUserId = localStorage.getItem("Id");
