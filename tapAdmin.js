1b3937790b692b3cd4c4dfce72624a98b8924bb1
Monisharp-dev <hubdickson@gmail.com>
2025-06-18 18:32:58 +0100

    Add files via upload

--- tapAdmin.js
+++ tapAdmin.js
@@ -28,6 +28,6 @@ function activateTapForUser(userId) {
 }
 
 // 👥 List of user Ids to assign
-const userIds = ["favourboy817","umohaniekeme42","michealopeyemi608","inansdaniel","blessingsalami081"];
+const userIds = ["favourboy817","umohaniekeme42","michealopeyemi608","inansdaniel","blessingsalami081","etukubonganthony"];
 
 userIds.forEach(id => activateTapForUser(id));
