9a11d11fbee8c6edbf2a76fb0cf3f64c0152fca0
Monisharp-dev <hubdickson@gmail.com>
2025-06-21 14:58:40 +0100

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -176,6 +176,7 @@ const navbarSidebarHTML = `
       <div class="sidebar-section">TASK</div>
       <nav class="nav-links">
           <a href="createTask.html"><i class="fas fa-plus-circle"></i> Create Task ${createTaskBadge}</a>
+          <a href="ad history.html"><i class="fas fa-history"></i> Task Creation History ${createTaskBadge}</a>
         <a href="task.html"><i class="fas fa-tasks"></i> Task</a>
         <a href="Taskwithdrawal.html"><i class="fas fa-wallet"></i> Task Withdrawal</a>
         <a href="withdraw.html"><i class="fas fa-hand-holding-usd"></i> Withdraw</a>
