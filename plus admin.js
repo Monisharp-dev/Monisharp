e2cb95bd8bbc57e8fbf45f83b03b9fc1396719e6
Monisharp-dev <hubdickson@gmail.com>
2025-09-02 21:54:57 +0100

    Add files via upload

--- plus admin.js
+++ plus admin.js
@@ -32,16 +32,54 @@ function allotTaskBalance(userId, amount, uniqueKey) {
   console.log(`✅ Task balance of ₦${amount} credited to ${userId} (Key: ${uniqueKey}).`);
 }
 // These all run, but only the one matching localStorage.getItem("plus-Id") will get credited
-allotTaskBalance("mohammedmanyisa9680plus", 50, "AUG87");
-allotTaskBalance("shikemiopeyemi0plus", 50, "EOZBS");
-allotTaskBalance("folarinagbesoyin19plus", 50, "EOZBS");
-allotTaskBalance("paulmaureen408plus", 200, "EOZBS");
-allotTaskBalance("olasunkanmiomotayo2", 18, "FZMLC");
-allotTaskBalance("amarachiwfauplus", 550, "FZMLC");
-allotTaskBalance("greatnessedet8plus", 1400, "FZZZC");
-allotTaskBalance("baloguntaiwo0001", 21, "SEPTE");
-allotTaskBalance("etokwudogplus", 50, "SEPTE");
-allotTaskBalance("inafisat019plus", 210, "FZSSC");
-allotTaskBalance("alexanderifenabohplus", 50, "FZMLC");
+allotTaskBalance("realearners01plus", 50, "O7JMD");
+allotTaskBalance("mubaraqmichaelplus", 100, "ERZBM");
+allotTaskBalance("theyplus", 600, "FZMLC");
+allotTaskBalance("tayefacebook2521plus", 350, "FD56T");
+allotTaskBalance("petersonroland57plus", 350, "DMXLC");
+allotTaskBalance("petersonroland57plus", 500, "GUTXG");
+allotTaskBalance("tayefacebook2521plus", 500, "GM66T");
+allotTaskBalance("pinodetommaso184plus", 500, "GMTID");
+allotTaskBalance("yope7028plus", 500, "DT665");
+allotTaskBalance("yope7028plus", 2000, "DBY45");
+ allotTaskBalance("mohammedmanyisa9680plus", 250, "FMT70");
+allotTaskBalance("tayefacebook2521plus", 500, "FWM56");
+allotTaskBalance("shikemiopeyemi0plus", 100, "GNM56");
+allotTaskBalance("tayefacebook2521plus", 150, "HTM76");
+allotTaskBalance("tayefacebook2521plus", 150, "FHA89");
+allotTaskBalance("mubaraqmichaelplus", 100, "DVH90");
+allotTaskBalance("yope7028plus", 50, "IJVED");
+allotTaskBalance("anigedavid9plus", 50, "DVE56");
+allotTaskBalance("pinodetommaso184plus", 250, "FUX76");
+allotTaskBalance("etokwudogplus", 250, "PTX90");
+allotTaskBalance("divineblandy009plus", 100, "HTX45");
+allotTaskBalance("etokwudogplus", 100, "TXL76");
+allotTaskBalance("tayefacebook2521plus", 100, "PGM89");
+allotTaskBalance("freshnessjunior1plus", 550, "FNL06");
+allotTaskBalance("anigedavid9plus", 350, "DLN23");
+allotTaskBalance("shikemiopeyemi0plus", 150, "BTX78");
+allotTaskBalance("anigedavid9plus", 50, "DHT89");
+allotTaskBalance("petersonroland57plus", 300, "FLN90");
+allotTaskBalance("freshnessjunior1plus", 200, "XML63");
+allotTaskBalance("mohammedmanyisa9680plus", 200, "HTL78");
+allotTaskBalance("etokwudogplus", 100, "NTG67");
+allotTaskBalance("anigedavid9plus", 100, "GTM76");
+allotTaskBalance("omurogo9plus", 250, "MRT70");
+allotTaskBalance("shikemiopeyemi0plus", 100, "BTH76");
+allotTaskBalance("folarinagbesoyin19plus", 550, "XNT90");
+allotTaskBalance("gfame682plus", 550, "GVT89");
+allotTaskBalance("winnerchukwuemeka70plus", 150, "MTU56");
+allotTaskBalance("iedidiong33plus", 500, "GFP99");
+allotTaskBalance("chizzeechisomplus", 50, "GZT09");
+allotTaskBalance("victoruduak2009plus", 550, "DHT70");
+allotTaskBalance("shanegray017plus", 350, "MGH89");
+allotTaskBalance("iedidiong33plus", 100, "DHM89");
+allotTaskBalance("shikemiopeyemi0plus", 100, "PLT70");
+allotTaskBalance("anigedavid9plus", 10, "MFN89");
+allotTaskBalance("miracle222fegaplus", 50, "HTM97");
+allotTaskBalance("alexanderifenabohplus", 50, "AHT90");
+allotTaskBalance("greatnessedet8plus", 1400, "AGM90");
 
-//hiii;
+
+
+
