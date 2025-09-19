4f4dbc482090d5ef0acdc548b9ea04847caf2a74
Monisharp-dev <hubdickson@gmail.com>
2025-09-19 16:06:25 +0100

    Add files via upload

--- deposit balance.js
+++ deposit balance.js
@@ -40,7 +40,7 @@ allotDepositBalance("sisfathia120", 500, "SETTX");
 allotDepositBalance("danemmanuel692plus", 200, "000SD");
 allotDepositBalance("thehacker190261", 6500, "LLLOP")
 allotDepositBalance("olasunkanmiomotayo2", 100, "0000A");
-allotDepositBalance("littlex40", 150,"000SD");
+allotDepositBalance("opeyemiola934", 500,"00DSD");
 allotDepositBalance("destinysylvanus74", 200, "000VB");
 allotDepositBalance("monisharp45", 509800, "0XXSG");
 allotDepositBalance("akpesiriedward5", 98, "000AB");
